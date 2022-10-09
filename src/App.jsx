11bdbfa4127a20ae986867824af179758986bfa2
Junayed01625 <junayed.miah@nrthsouth.edu>
2022-10-10 01:31:22 +0600

    second

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from './components/header/header.jsx'
+import Nav from './components/nav/nav.jsx'
+import About from './components/about/about.jsx'
+import Experience from './components/experience/experience.jsx'
+import Services from './components/services/services.jsx'
+import Portfolio from './components/portfolio/portfolio.jsx'
+import Testimonial from './components/testimonials/testimonial.jsx'
+import Contact from './components/contact/contact.jsx'
+import Footer from './components/footer/footer.jsx'
+
+export const App = () => {
+  return (
+    <>
+      <Header/>
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonial />
+      <Contact />
+      <Footer />
+
+    </>
+  )
+}
---
+++ src/components/header/headersocial.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {FiDribbble} from 'react-icons/fi'
+import {BsFacebook} from 'react-icons/bs'
+
+const headersocial = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><BsFacebook/></a>
+        <a href="https://dribble.com" target="_blank" rel="noopener noreferrer"><FiDribbble/></a>
+    </div>
+  )
+}
+
+export default headersocial
