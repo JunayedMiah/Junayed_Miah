7ad62b354a791cfe13083e8b91cea69a954b62bd
Junayed01625 <junayed.miah@nrthsouth.edu>
2022-10-14 21:41:31 +0600

    final

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './footer.css'
-const footer = () => {
+import{BsFacebook} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs'
+
+const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>JUNAYED</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonial;">Testimonial</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"><BsFacebook/></a>
+        <a href="https://instragram.com"><BsInstagram/></a>
+        <a href="https://linkedin.com"><BsLinkedin/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Md Junayed Miah. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
-export default footer
+export default Footer
