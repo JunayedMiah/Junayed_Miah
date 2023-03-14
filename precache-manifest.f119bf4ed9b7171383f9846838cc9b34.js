d8ba33218e52845da60ed20d59cea0212f9f7fb5
Md Junayed Miah <44528101+JunayedMiah@users.noreply.github.com>
2023-03-14 13:28:36 +0600

    Updates

---
+++ precache-manifest.f119bf4ed9b7171383f9846838cc9b34.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "1996977554760d7ba6d3",
+    "url": "/Junayed_Miah/static/css/main.d5da93fa.chunk.css"
+  },
+  {
+    "revision": "1996977554760d7ba6d3",
+    "url": "/Junayed_Miah/static/js/main.19969775.chunk.js"
+  },
+  {
+    "revision": "2abacf679992562afa32",
+    "url": "/Junayed_Miah/static/css/1.8e9990be.chunk.css"
+  },
+  {
+    "revision": "2abacf679992562afa32",
+    "url": "/Junayed_Miah/static/js/1.2abacf67.chunk.js"
+  },
+  {
+    "revision": "0ae8266de2e26575cc41",
+    "url": "/Junayed_Miah/static/js/runtime~main.0ae8266d.js"
+  },
+  {
+    "revision": "a97876048a7239da7ec16299da33a2a9",
+    "url": "/Junayed_Miah/static/media/CV.a9787604.pdf"
+  },
+  {
+    "revision": "479bf96bf49864a56166a02c759ead81",
+    "url": "/Junayed_Miah/static/media/ridoy.479bf96b.jpg"
+  },
+  {
+    "revision": "7d1fe24b3072b92a2bd11f8a5d569f9b",
+    "url": "/Junayed_Miah/static/media/me-about.7d1fe24b.jpg"
+  },
+  {
+    "revision": "e1ee86df3e0a44b33111605f940299a2",
+    "url": "/Junayed_Miah/static/media/img-1.e1ee86df.png"
+  },
+  {
+    "revision": "c54eb8594d8d65ed73dff82e3c863356",
+    "url": "/Junayed_Miah/static/media/img-2.c54eb859.png"
+  },
+  {
+    "revision": "97431b1dc78659f8c23c6bf5cab54fc8",
+    "url": "/Junayed_Miah/static/media/img-3.97431b1d.png"
+  },
+  {
+    "revision": "bde72a0663d8a29b1b3e450d0a7176f6",
+    "url": "/Junayed_Miah/static/media/img-4.bde72a06.png"
+  },
+  {
+    "revision": "bb36cce37afc2393a465f8ec75ba041f",
+    "url": "/Junayed_Miah/static/media/img-5.bb36cce3.png"
+  },
+  {
+    "revision": "f4532e71a2a5ba5a44e94659ef478f99",
+    "url": "/Junayed_Miah/static/media/img-6.f4532e71.png"
+  },
+  {
+    "revision": "efb57aca6dd10300253e8856c29930fe",
+    "url": "/Junayed_Miah/index.html"
+  }
+];
